83d9d2dae7d0e4663f248ff1de30f30bacde5dd3
CristiMcMc11 <crimunt0@gmail.com>
2026-01-26 19:40:56 -0800

    cinemachine + camera
    good enough camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform player;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, -10);
+    }
+}
